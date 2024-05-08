f40c1dec77e6fe62ddf7a8378d929a1bbd93cf9f
Mtalhaa832 <mtalhaa832@gmail.com>
2024-05-08 06:52:21 +0500

    a

---
+++ index.js
@@ -0,0 +1,7 @@
+let input =document.getElementById("input")
+let p = document.getElementById("p");
+let button =document.getElementById("button");
+button.addEventListener("click",function(){
+let x =window.location.assign("indexa.html");
+p.innerHTML = input.value
+})
